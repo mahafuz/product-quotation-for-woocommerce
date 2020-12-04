0e65125f25d1cc416e2daec0740e2b7dc7471d4a
Mahafuz <m.mahfuz.me@gmail.com>
2020-12-04 19:58:57 +0600

    Added: WooCommerce Notice

--- includes/Bootstrap.php
+++ includes/Bootstrap.php
@@ -93,6 +93,10 @@ class Bootstrap {
 		$this->form_handler  = Form_Handler::instance();
 		$this->settings      = Settings::instance();
 
+		if ( ! function_exists( 'WC' ) ) {
+			add_action( 'admin_notices', array ( $this, 'woocommerce_not_loaded' ) );
+		}
+
 	}
 
 	/**
@@ -110,6 +114,53 @@ class Bootstrap {
 		}
 
 		return wp_nonce_url( admin_url( $url ), 'pqfw_admin_nonce_action', '_wpnonce' );
+
+	}
+
+	/**
+	 * Check if a plugin is installed
+	 *
+	 * @since 1.0.0
+	 */
+	public function is_plugin_installed( $basename ) {
+		if ( ! function_exists( 'get_plugins' ) ) {
+			include_once ABSPATH . '/wp-admin/includes/plugin.php';
+		}
+
+		$installed_plugins = get_plugins();
+
+		return isset( $installed_plugins[ $basename ] );
+	}
+
+	/**
+	 * Check if woocommerce plugin is activated
+	 *
+	 * @since v1.0.0
+	 */
+	public function woocommerce_not_loaded() {
+
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+
+		$woocommerce = 'woocommerce/woocommerce.php';
+
+		if ( $this->is_plugin_installed( $woocommerce ) ) {
+			$activation_url = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $woocommerce . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $woocommerce );
+
+			$message = sprintf( __( '%1$sProduct Quotation For WooCommerce%2$s requires %1$sWooCommerce%2$s plugin to be active. Please activate WooCommerce to continue.', 'pqfw' ), "<strong>", "</strong>" );
+
+			$button_text = __( 'Activate WooCommerce', 'pqfw' );
+		} else {
+			$activation_url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=woocommerce' ), 'install-plugin_woocommerce' );
+
+			$message     = sprintf( __( '%1$sProduct Quotation For WooCommerce%2$s requires %1$sWooCommerce%2$s plugin to be installed and activated. Please install WooCommerce to continue.', 'pqfw' ), '<strong>', '</strong>' );
+			$button_text = __( 'Install WooCommerce', 'pqfw' );
+		}
+
+		$button = '<p><a href="' . $activation_url . '" class="button-primary">' . $button_text . '</a></p>';
+
+		printf( '<div class="error"><p>%1$s</p>%2$s</div>', __( $message ), $button );
 
 	}
 
