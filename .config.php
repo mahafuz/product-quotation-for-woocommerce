790d4e393505d02d5e7b66f4f8a2452b37a6bebf
mahafuz <m.mahfuz.me@gmail.com>
2026-05-02 05:05:54 +0600

    [Remove] config.php

--- .config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define( 'WP_FS__DEV_MODE', true );
-define( 'WP_FS__SKIP_EMAIL_ACTIVATION', true );
-define( 'WP_FS__quotify_SECRET_KEY', 'sk_hXBRBC?sbAIeow)-S3guRBw]iwC<o' );//phpcs:ignore
--- product-quotation-for-woocommerce.php
+++ product-quotation-for-woocommerce.php
@@ -45,11 +45,6 @@ if ( ! class_exists( 'Quotify', false ) ) {
 	include_once dirname( QUOTIFY_PLUGIN_FILE ) . '/app/quotify.php';
 }
 
-$configs = __DIR__ . '/.config.php';
-if ( WP_DEBUG && file_exists( $configs ) ) {
-	require $configs;
-}
-
 require __DIR__ . '/app/install.php';
 
 if ( ! function_exists( 'quotify' ) ) {
